a42a673587f680dd1db372a215c9b2d6ed175317
Lireoy <34489456+Lireoy@users.noreply.github.com>
2020-06-18 19:26:55 +0200

    More clean codify: WhoCanUseMe command

--- src/main/java/bot/commands/informative/WhoCanUseMe.java
+++ src/main/java/bot/commands/informative/WhoCanUseMe.java
@@ -32,17 +32,58 @@ public class WhoCanUseMe extends Command {
         final User commandAuthor = commandEvent.getAuthor();
         log.info("Command ran by {}", commandAuthor);
 
-        final String[] commandExamples = {BotConstants.PREFIX + this.name};
-
-        final boolean helpResponse = HelpUtil.getInstance()
-                .sendCommandHelp(this, commandEvent, commandExamples);
-        if (helpResponse) return;
+        if (checkHelp(commandEvent)) return;
 
         List<Role> roleWithManageServer = new ArrayList<>();
         List<Role> roleToUse = new ArrayList<>();
+        collectRoleInfo(commandEvent, roleWithManageServer, roleToUse);
 
+        StringBuilder description = new StringBuilder();
+        if (roleWithManageServer.size() < 1 && roleToUse.size() < 1) {
+            commandEvent.reply(Locale.WHO_CAN_USE_ME_COMMAND_NO_ROLES);
+            return;
+        }
+
+        formatRolesWithManageServer(roleWithManageServer, description);
+        if (roleWithManageServer.size() > 0 && roleToUse.size() > 0) {
+            description.append("\n").append("=================================\n\n");
+        }
+        formatRolesWithSimpleUse(roleToUse, description);
+
+        commandEvent.reactSuccess();
+        commandEvent.replyFormatted(description.toString());
+    }
+
+    private void formatRolesWithSimpleUse(List<Role> roleToUse, StringBuilder description) {
+        if (roleToUse.size() > 0) {
+            description.append(Locale.WHO_CAN_USE_ME_COMMAND_BASIC_COMMANDS);
+            for (Role role : roleToUse) {
+                if (role.isManaged()) {
+                    description.append(String.format(Locale.WHO_CAN_USE_ME_COMMAND_ADVANCED_LINE, role.getId()));
+                } else {
+                    description.append(String.format(Locale.WHO_CAN_USE_ME_COMMAND_SIMPLE_LINE, role.getId()));
+                }
+            }
+        }
+    }
+
+    private void formatRolesWithManageServer(List<Role> roleWithManageServer, StringBuilder description) {
+        if (roleWithManageServer.size() > 0) {
+            description.append(Locale.WHO_CAN_USE_ME_COMMAND_MANAGE_NOTIFS);
+            for (Role role : roleWithManageServer) {
+                if (role.isManaged()) {
+                    description.append(String.format(Locale.WHO_CAN_USE_ME_COMMAND_ADVANCED_LINE, role.getId()));
+                } else {
+                    description.append(String.format(Locale.WHO_CAN_USE_ME_COMMAND_SIMPLE_LINE, role.getId()));
+                }
+            }
+        }
+    }
+
+    private void collectRoleInfo(CommandEvent commandEvent, List<Role> roleWithManageServer, List<Role> roleToUse) {
         for (Role role : commandEvent.getGuild().getRoles()) {
-            if (role.getPermissions().contains(Permission.MANAGE_SERVER) || role.getPermissions().contains(Permission.ADMINISTRATOR)) {
+            if (role.getPermissions().contains(Permission.MANAGE_SERVER) ||
+                    role.getPermissions().contains(Permission.ADMINISTRATOR)) {
                 roleWithManageServer.add(role);
             }
 
@@ -50,52 +91,10 @@ public class WhoCanUseMe extends Command {
                 roleToUse.add(role);
             }
         }
+    }
 
-        StringBuilder description = new StringBuilder();
-
-        if (roleWithManageServer.size() < 1 && roleToUse.size() < 1) {
-            commandEvent.reply(Locale.WHO_CAN_USE_ME_COMMAND_NO_ROLES);
-            return;
-        }
-
-        if (roleWithManageServer.size() > 0) {
-            description.append(Locale.WHO_CAN_USE_ME_COMMAND_MANAGE_NOTIFS);
-            for (Role role : roleWithManageServer) {
-                if (role.isManaged()) {
-                    description.append(
-                            String.format(
-                                    Locale.WHO_CAN_USE_ME_COMMAND_ADVANCED_LINE,
-                                    role.getId()));
-                } else {
-                    description.append(
-                            String.format(
-                                    Locale.WHO_CAN_USE_ME_COMMAND_SIMPLE_LINE,
-                                    role.getId()));
-                }
-            }
-        }
-
-        if (roleWithManageServer.size() > 0 && roleToUse.size() > 0)
-            description.append("\n").append("=================================\n\n");
-
-        if (roleToUse.size() > 0) {
-            description.append(Locale.WHO_CAN_USE_ME_COMMAND_BASIC_COMMANDS);
-            for (Role role : roleToUse) {
-                if (role.isManaged()) {
-                    description.append(
-                            String.format(
-                                    Locale.WHO_CAN_USE_ME_COMMAND_ADVANCED_LINE,
-                                    role.getId()));
-                } else {
-                    description.append(
-                            String.format(
-                                    Locale.WHO_CAN_USE_ME_COMMAND_SIMPLE_LINE,
-                                    role.getId()));
-                }
-            }
-        }
-
-        commandEvent.reactSuccess();
-        commandEvent.replyFormatted(description.toString());
+    private boolean checkHelp(CommandEvent commandEvent) {
+        final String[] commandExamples = {BotConstants.PREFIX + this.name};
+        return HelpUtil.getInstance().sendCommandHelp(this, commandEvent, commandExamples);
     }
 }
